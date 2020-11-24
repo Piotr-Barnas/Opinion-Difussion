c1b75c24e2b05ff84560041112bee8790d44ba13
Piotr-Barnas <piotr.barnas13@gmail.com>
2020-11-25 00:45:55 +0100

    Approach with undirected and unweighted graphs and with mails_no. thresholds

--- new_code3.py
+++ new_code3.py
@@ -6,12 +6,14 @@ Created on Sat Nov  7 18:49:44 2020
 @author: barni13
 """
 
+
 import pandas as pd
 import igraph as ig
 import numpy as np
-import leidenalg
+import louvain
 import itertools as itl
 from copy import deepcopy
+import matplotlib.pyplot as plt
 
 #functions
 def is_integer(n):
@@ -23,9 +25,10 @@ def is_integer(n):
     else:
         return float(n).is_integer()
     
-def find_best_new_edge(graph, n):
-    """finds among all possible pairs n edges with maxiumum modularity"""
-    pairs = list(itl.permutations(graph.vs['name'], 2))
+def find_best_new_edge(graph = g, n = 15, k = 3):
+    """finds among all possible pairs, n edges with maxiumum modularity.
+       recalculating modularities after adding k best edges"""
+    pairs = list(itl.combinations(graph.vs['name'], 2))
     edges_in_graph = [(x['name'], y['name']) for (x, y) in [e.vertex_tuple for e in graph.es]]
     for pair in edges_in_graph:
         pairs.remove(pair)
@@ -39,15 +42,46 @@ def find_best_new_edge(graph, n):
         modularities = []
         for m in range(len(pairs)):
             new_graph = deepcopy(graph)
-            new_graph.add_edge(pairs[m][0], pairs[m][1], weights = np.mean(graph.es['weights']))
-            modularities.append(leidenalg.find_partition(new_graph, leidenalg.ModularityVertexPartition).quality())
-        print(modularities)
-        graph.add_edge(pairs[modularities.index(max(modularities))][0], pairs[modularities.index(max(modularities))][1], weights = np.mean(graph.es['weights']))
-        best_pairs.append(pairs[modularities.index(max(modularities))])
-        best_modularities.append(max(modularities))
-        pairs.remove(pairs[modularities.index(max(modularities))])
-        i = i + 1
-    return (best_pairs, best_modularities)
+            new_graph.add_edge(pairs[m][0], pairs[m][1])
+            modularities.append(louvain.find_partition(new_graph, louvain.ModularityVertexPartition).quality())
+        if n - i > k:
+            for _ in range(k):
+                graph.add_edge(pairs[modularities.index(max(modularities))][0], pairs[modularities.index(max(modularities))][1])
+                best_pairs.append(pairs[modularities.index(max(modularities))])
+                best_modularities.append(max(modularities))
+                pairs.remove(pairs[modularities.index(max(modularities))])
+                modularities.remove(max(modularities))
+                i += 1
+        else:
+            for _ in range(n - i):
+                graph.add_edge(pairs[modularities.index(max(modularities))][0], pairs[modularities.index(max(modularities))][1])
+                best_pairs.append(pairs[modularities.index(max(modularities))])
+                best_modularities.append(max(modularities))
+                pairs.remove(pairs[modularities.index(max(modularities))])
+                modularities.remove(max(modularities))
+                i += i      
+    return (best_pairs, best_modularities, i)
+
+def independent_cascades(graph, initial):
+    k = 0
+    arch = [(initial, k)]
+    all_influenced = initial
+    influenced_in_k = initial
+    for emp in initial:
+        graph.vs.select(name = emp)['influenced'] = 1
+    while influenced_in_k != [] and k < 10:
+        k = k + 1
+        influenced_in_k = []
+        for emp in initial:
+            for ngbr in [graph.vs.select(x)['name'][0] for x in graph.neighbors(emp, mode = 'out')]:
+                if ngbr not in all_influenced:
+                    rand = np.random.uniform()
+                    if rand < 0.5:
+                        influenced_in_k.append(ngbr)
+                        all_influenced.append(ngbr)
+                        graph.vs.select(name = ngbr)['influenced'] = 1
+        arch.append((influenced_in_k, k))                
+    return arch, all_influenced    
     
 
 
@@ -60,7 +94,7 @@ print(reportsto)
 #convert to index with start at 0
 communication['Sender'] = communication['Sender'] - 1
 communication['Recipient'] = communication['Recipient'] - 1
-
+ 
 reportsto['ID'] = reportsto['ID'] - 1
 reportsto['ReportsToID'] = [int(x) - 1 if is_integer(x) else x for x in reportsto['ReportsToID']]
 
@@ -87,37 +121,71 @@ weights = mail_counter/mail_counter.sum(axis=1, keepdims=True)
 weights = np.nan_to_num(weights)
 
 #graph, vertices and edges
-g = ig.Graph(directed = True)
+g = ig.Graph()
+
 
 for emp in reportsto['ID']:
     g.add_vertex(name = str(emp))
 
+list_both_sides = []
 for k in range(0, len(reportsto['ID'])):
-    for l in range(0, len(reportsto['ID'])):
-        if k == l:
+    for l in range(k + 1, len(reportsto['ID'])):
+        if mail_counter[k][l] == 0 or mail_counter[l][k] == 0:
             pass
-        elif weights[k][l] != 0:
-            g.add_edge(str(k), str(l), weights = weights[k][l])
+        else:
+            both_sides = mail_counter[k][l] + mail_counter[l][k]
+            list_both_sides.append(both_sides)
+            if both_sides >= 30:
+                g.add_edge(str(k), str(l))
+            
+##########################################################################################        
+#                                ustalanie thresholdu                                    #
+##########################################################################################
+"""
+thresholds = []
+above_trsh = []        
+for threshold in np.linspace(min(list_both_sides), max(list_both_sides), 1000):
+    thresholds.append(threshold)
+    above_trsh.append(len([x for x in list_both_sides if x >= threshold]))
+
+plt.figure()
+plt.plot(thresholds, [x / len(g.vs['name']) for x in above_trsh])
+
+[x / len(g.vs['name']) for x in above_trsh].index(2)
+
+plt.figure()
+plt.plot(thresholds[0:18], [x / len(g.vs['name']) for x in above_trsh][0:18])
+"""
+#########################################################################################
+
 
 g.delete_vertices(ndls_ids) #verices of former emp/tech. acc. deletion
 
 #difinig of plots visual style
-layout = g.layout("tree") #tree / grid_fr      kk (?)
+color_dict = ['red', 'blue']
+layout = g.layout("fr") #tree / grid_fr / fr / kk / rt_circular
 visual_style = {}
-visual_style["vertex_size"] = 20
-#visual_style["vertex_color"] = [color_dict[gender] for gender in g.vs["gender"]]
-#visual_style["vertex_label"] = g.vs["name"]
-#visual_style["edge_width"] = [1 + 2 * int(is_formal) for is_formal in g.es["is_formal"]]
+visual_style["vertex_size"] = 50
+#visual_style["vertex_color"] = [color_dict[int(x == 0)] for x in g.vs.degree()]
+visual_style["vertex_label"] = g.vs["name"]
+#["edge_length"] = 200
+visual_style["edge_width"] = 1
 visual_style["layout"] = layout
-visual_style["bbox"] = (1200, 1200)
-visual_style["margin"] = 20
+visual_style["bbox"] = (3000, 2000)
+visual_style["margin"] = 50
 
-#ig.plot(g, **visual_style).show()
+disconnected = [x['name'] for x in g.vs if x.degree() == 0]
+g2 = deepcopy(g)
+g2.delete_vertices(disconnected)
+
+ig.plot(g2, **visual_style).save("/home/barni13/Desktop/Studia/Semestr 7/Praca Dyplomowa/initial_graph.png")
 
 #partition of base graph
-partition = leidenalg.find_partition(g, leidenalg.ModularityVertexPartition)
-print(partition.quality)
-#ig.plot(partition, **visual_style).show()
+partition = louvain.find_partition(g2, louvain.ModularityVertexPartition)
+print(partition.quality())
+ig.plot(partition, **visual_style).save("/home/barni13/Desktop/Studia/Semestr 7/Praca Dyplomowa/partition_graph.png")
+
+
 
 """
 #edge appending
@@ -133,4 +201,5 @@ max(modularities)
 pairs[modularities.index(max(modularities))]
 """
 
-find_best_new_edge(g, 5)
+find_best_new_edge(g, 20, 3) 
+
